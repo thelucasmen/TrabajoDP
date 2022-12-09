d99a88e88734eb01561b6d5a74eecacff209786d
MarcosAcedoMejías <55592990+MarcosAcedo@users.noreply.github.com>
2022-12-09 14:01:07 +0100

    interfaz Ciclista

--- Ciclista.java
+++ Ciclista.java
@@ -6,198 +6,52 @@ import java.util.*;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Ciclista
+interface Ciclista
 {
     // instance variables - replace the example below with your own
-    private String nombre;
-    private Bicicleta bicicleta;
-    private Habilidad habilidad;
-    private double energia;
-    private List<Resultado> resultados;
-    private Equipo equipo;
 
     /**
      * Constructor for objects of class Ciclista
      */
+    public void setNombre(String n);
+    public String getNombre();
     
-    public Ciclista(String nombre, Bicicleta bicicleta, Habilidad habilidad, double energia, List<Resultado> resultado, Equipo equipo)
-    {
-        this.nombre = nombre;
-        this.bicicleta = bicicleta;
-        this.habilidad = habilidad;
-        this.energia = energia;
-        this.resultados = new ArrayList<Resultado>();
-        this.equipo = equipo;
-    }
+    public void setBicicleta(Bicicleta b);
+    public Bicicleta getBicicleta();
     
-    public Ciclista(String nombre, Habilidad habilidad, double energia, Equipo equipo)
-    {
-        this.nombre = nombre;
-        this.habilidad = habilidad;
-        this.energia = energia;
-        this.equipo = equipo;
-    }
+    public void setHabilidad(Habilidad h);
+    public Habilidad getHabilidad();
+    
+    public void setEnergia(double e);
+    public double getEnergia();
+        
+    public void setResultado(Resultado r);
+    public void getResultado();
+    
+    public void setEquipo(Equipo e);
+    public Equipo getEquipo();
+    
+    public void mostrar();
 
-    public void setNombre(String n){
-        this.nombre = n;
-    }
-    
-    public void setBicicleta(Bicicleta b){
-        this.bicicleta = b;
-    }
-    
-    public void setHabilidad(Habilidad h){
-        this.habilidad = h;
-    }
-    
-    public void setEnergia(double e){
-        this.energia = e;
-    }
-        
-    public void setResultado(Resultado r){
-        resultados.add(r);   
-    }
-    
-    public void getResultado(){ 
-        System.out.println("Resultados: ");
-        Iterator<Resultado> it = resultados.iterator();     
-        while(it.hasNext()) {
-          it.next().mostrar();
-        }              
-    }
-    
-    public void setEquipo(Equipo e){
-        this.equipo = e;
-    }
-    
-    public String getNombre(){
-        return nombre;
-    }
-    
-    public Bicicleta getBicicleta(){
-        return bicicleta;
-    }
-    
-    public Habilidad getHabilidad(){
-        return habilidad;
-    }
-    
-    public double getEnergia(){
-        return energia;
-    }
-    
-    public Equipo getEquipo(){
-        return equipo;
-    }
-        
-    public void mostrar() { }
-    
-        /*Funcionalidad */
-    //Cambiar la bicicleta
-    public void cambiarBici(Bicicleta bicicleta){
-        setBicicleta(bicicleta);
-    }
-    
-    //Comprueba si el ciclista ha abandonado
-    public boolean abandonado(){
-        if (energia <= 0){
-            return true;
-        } else{
-            return false;
-        }
-    }
-    
-    //Muestra resultados de cualquier etapa
-    public Resultado mostrarResultados(Etapa e){
-        //Busca una etapa concreta dentro del Array y devuelve la información del resultado de esa etapa, si no, devuelve null.
-        Resultado aux = null;
-        boolean enc = false;
-        int i = 0;       
-        Iterator<Resultado> it = resultados.iterator();
-        
-        while (it.hasNext() && !enc) {
-            if (it.next().getEtapa() == e){
-                aux = it.next();
-                enc = true;
-            }
-            i++;
-        }
-        if(enc==true)
-            return aux;
-        else
-            return null;
-    }
-    
-    //Devuelve el numro de etapas
-    public int numeroEtapas(){
-        return resultados.size();
-    }
-    
-    //Devuelve el tiempo total acumulado entre los resultados
-    public double tiempoAcumulado(){
-        double tiempo = 0.0;
-        try{
-            Iterator<Resultado> it = resultados.iterator();
-            
-            while (it.hasNext()) {
-                if(it.next().getTiempo() > 0){
-                    tiempo = tiempo + it.next().getTiempo();
-                }
-            }
-        }catch(NullPointerException e){ }
-        return tiempo;
-    }
-    
-    //Numero de etapas terminadas
-    public int etapasTerminadas(){
-        int cont = 0;
-        Iterator<Resultado> it = resultados.iterator();
-        
-        while (it.hasNext()) {
-            if(it.next().getTiempo() > 0){
-                cont++;
-            }
-        }
-        return cont;
-        
-    }
-    
-    //Muestra por pantalla las etapas abandonadas
-    public String abandonada(){
-        Iterator<Resultado> it = resultados.iterator();
-        String stringAbandonadas = "";
-        
-        while (it.hasNext()) {
-            if(it.next().getTiempo() <= 0){
-                stringAbandonadas += "/n/t" + it.next().getEtapa();
-            }
-        }
-        return stringAbandonadas;
-    }
-    
-    //Numero de etapas comenzadas, terminadas y tiempo acumulado, etapas abandonadas
-    public String mostrarEtapas(){
-        return "Etapas comenzadas:" + numeroEtapas() 
-                    + "/nTiempo acumulado en etapas terminadas: " + tiempoAcumulado() 
-                    + "/nEtapas terminadas:" + etapasTerminadas() 
-                    + "/nEtapas abandonadas:" + abandonada();
-    }
-                       
-    //Guarda en resultados el tiempo        tiempo=bicicleta.calculartiempo(ciclista,etapa);
-    public void funcionalidadCiclista(Etapa e, double tiempo){
-        Resultado r;
-        Bicicleta b = getBicicleta();
-        if(getEnergia() != 0){
-            r = new Resultado(e, tiempo);
-            setEnergia(tiempo/60);
-        } else {
-            r = new Resultado(e, energia - tiempo);
-        }
-        r.setEtapa(e);
-        resultados.add(r);
-    }
-    
-    public double calculaDestreza(){
-        return 1;
-    }
+    /***Funcionalidad***/
+        //Cambiar la bicicleta
+    public void cambiarBici(Bicicleta bicicleta);
+        //Comprueba si el ciclista ha abandonado
+    public boolean abandonado();
+        //Muestra resultados de cualquier etapa
+    public Resultado mostrarResultados(Etapa e);
+        //Devuelve el numro de etapas
+    public int numeroEtapas();
+        //Devuelve el tiempo total acumulado entre los resultados
+    public double tiempoAcumulado();
+        //Numero de etapas terminadas
+    public int etapasTerminadas();
+        //Muestra por pantalla las etapas abandonadas
+    public String abandonada();
+        //Numero de etapas comenzadas, terminadas y tiempo acumulado, etapas abandonadas
+    public String mostrarEtapas();
+        //Guarda en resultados el tiempo        tiempo=bicicleta.calculartiempo(ciclista,etapa);
+    public void funcionalidadCiclista(Etapa e, double tiempo);
+        //Caclcula la destreza del ciclista
+    public double calculaDestreza();
 }
--- CiclistaEstrella.java
+++ CiclistaEstrella.java
@@ -6,7 +6,7 @@ import java.util.*;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class CiclistaEstrella extends Ciclista 
+public class CiclistaEstrella implements Ciclista 
 {
     // instance variables - replace the example below with your own
     private String nombre;
@@ -22,13 +22,15 @@ public class CiclistaEstrella extends Ciclista
      */
     public CiclistaEstrella(String nombre, Bicicleta bicicleta, Habilidad habilidad, double energia, List<Resultado> resultado, Equipo equipo)
     {
-       super(nombre, bicicleta, habilidad, energia, resultado, equipo);
+       this.nombre = nombre;
+       this.habilidad = habilidad;
+       this.energia = energia;
+       this.equipo = equipo;
        this.popularidad = 6;       
     }
     
     public CiclistaEstrella(String nombre, Habilidad habilidad, double energia, Equipo equipo)
     {
-       super(nombre, habilidad, energia, equipo);
        this.nombre = nombre;
        this.habilidad = habilidad;
        this.energia = energia;
@@ -42,6 +44,163 @@ public class CiclistaEstrella extends Ciclista
     
     public int getPopularidad(){
         return popularidad;
+    }
+
+    public void setNombre(String n){
+        this.nombre = n;
+    }
+    
+    public void setBicicleta(Bicicleta b){
+        this.bicicleta = b;
+    }
+    
+    public void setHabilidad(Habilidad h){
+        this.habilidad = h;
+    }
+    
+    public void setEnergia(double e){
+        this.energia = e;
+    }
+        
+    public void setResultado(Resultado r){
+        resultados.add(r);   
+    }
+    
+    public void getResultado(){ 
+        System.out.println("Resultados: ");
+        Iterator<Resultado> it = resultados.iterator();     
+        while(it.hasNext()) {
+          it.next().mostrar();
+        }              
+    }
+    
+    public void setEquipo(Equipo e){
+        this.equipo = e;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public Bicicleta getBicicleta(){
+        return bicicleta;
+    }
+    
+    public Habilidad getHabilidad(){
+        return habilidad;
+    }
+    
+    public double getEnergia(){
+        return energia;
+    }
+    
+    public Equipo getEquipo(){
+        return equipo;
+    }
+    
+        /*Funcionalidad */
+    //Cambiar la bicicleta
+    public void cambiarBici(Bicicleta bicicleta){
+        setBicicleta(bicicleta);
+    }
+    
+    //Comprueba si el ciclista ha abandonado
+    public boolean abandonado(){
+        if (energia <= 0){
+            return true;
+        } else{
+            return false;
+        }
+    }
+    
+    //Muestra resultados de cualquier etapa
+    public Resultado mostrarResultados(Etapa e){
+        //Busca una etapa concreta dentro del Array y devuelve la información del resultado de esa etapa, si no, devuelve null.
+        Resultado aux = null;
+        boolean enc = false;
+        int i = 0;       
+        Iterator<Resultado> it = resultados.iterator();
+        
+        while (it.hasNext() && !enc) {
+            if (it.next().getEtapa() == e){
+                aux = it.next();
+                enc = true;
+            }
+            i++;
+        }
+        if(enc==true)
+            return aux;
+        else
+            return null;
+    }
+    
+    //Devuelve el numro de etapas
+    public int numeroEtapas(){
+        return resultados.size();
+    }
+    
+    //Devuelve el tiempo total acumulado entre los resultados
+    public double tiempoAcumulado(){
+        double tiempo = 0.0;
+        try{
+            Iterator<Resultado> it = resultados.iterator();
+            
+            while (it.hasNext()) {
+                if(it.next().getTiempo() > 0){
+                    tiempo = tiempo + it.next().getTiempo();
+                }
+            }
+        }catch(NullPointerException e){ }
+        return tiempo;
+    }
+    
+    //Numero de etapas terminadas
+    public int etapasTerminadas(){
+        int cont = 0;
+        Iterator<Resultado> it = resultados.iterator();
+        
+        while (it.hasNext()) {
+            if(it.next().getTiempo() > 0){
+                cont++;
+            }
+        }
+        return cont;
+        
+    }
+    
+    //Muestra por pantalla las etapas abandonadas
+    public String abandonada(){
+        Iterator<Resultado> it = resultados.iterator();
+        String stringAbandonadas = "";
+        
+        while (it.hasNext()) {
+            if(it.next().getTiempo() <= 0){
+                stringAbandonadas += "/n/t" + it.next().getEtapa();
+            }
+        }
+        return stringAbandonadas;
+    }
+    
+    //Numero de etapas comenzadas, terminadas y tiempo acumulado, etapas abandonadas
+    public String mostrarEtapas(){
+        return "Etapas comenzadas:" + numeroEtapas() 
+                    + "/nTiempo acumulado en etapas terminadas: " + tiempoAcumulado() 
+                    + "/nEtapas terminadas:" + etapasTerminadas() 
+                    + "/nEtapas abandonadas:" + abandonada();
+    }
+                       
+    //Guarda en resultados el tiempo        tiempo=bicicleta.calculartiempo(ciclista,etapa);
+    public void funcionalidadCiclista(Etapa e, double tiempo){
+        Resultado r;
+        Bicicleta b = getBicicleta();
+        if(getEnergia() != 0){
+            r = new Resultado(e, tiempo);
+            setEnergia(tiempo/60);
+        } else {
+            r = new Resultado(e, energia - tiempo);
+        }
+        r.setEtapa(e);
+        resultados.add(r);
     }
     
     public void mostrar()
